6925dc70d8e9540bddf4170a30ce4a522aaee09d
Sarah Deleppo <sarah.deleppo@gmail.com>
2024-06-30 22:52:34 -0400

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExpenseTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ExpensesDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ExpensesDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+
+            var totalExpenses = allExpenses.Sum(x => x.Value);
+            ViewBag.Expense = totalExpenses;
+
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if (id != null)
+            {
+                //if id is supplied, load expense by id
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }
+            return View();
+        }
+        public IActionResult DeleteExpense(int id)
+        {
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            if (model.Id == 0)
+            {
+                //create
+                _context.Expenses.Add(model);
+
+            } else
+            {
+                //edit
+                _context.Expenses.Update(model);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
